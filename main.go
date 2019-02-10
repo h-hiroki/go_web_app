2b6ba21464c234381b88ccb5489ce5d87f18fe91
hhiroki <h-hiroki@users.noreply.github.com>
2019-02-10 13:41:51 +0900

    ⛳️特定パスで402エラーを返却する様に

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	"net/http"
 )
 
+func paidHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "You are paid\n")
+}
+
+func notPayHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusPaymentRequired) // httpには各HTTPステータスが定数で定義されている！
+	fmt.Fprint(w, "👯‍♀️Payment Required👯‍♀️\n")
+}
+
 func main() {
-	// ルートにアクセスしたときに Hello Worldを返却する
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
+	// /paidにアクセスした場合の処理
+	http.HandleFunc("/paid", paidHandler)
+	// /not_payにアクセスした場合の処理
+	http.HandleFunc("/not_pay", notPayHandler)
 
 	// 第一引数 "ホスト名:ポート番号" ホスト名を省略するとlocalhostになる
 	// 第二引数 HTTPハンドラを指定する。nilの場合はDefaultServeMuxが使われる
