ac55f31b27a4618365993aca96db7a34403ceb8c
hhiroki <h-hiroki@users.noreply.github.com>
2019-02-10 12:41:11 +0900

    ⛳️Hello Worldを返す

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	// ルートにアクセスしたときに Hello Worldを返却する
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
 	// 第一引数 "ホスト名:ポート番号" ホスト名を省略するとlocalhostになる
 	// 第二引数 HTTPハンドラを指定する。nilの場合はDefaultServeMuxが使われる
 	http.ListenAndServe(":8080", nil)
